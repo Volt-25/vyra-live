40ac0b4df0936868bf9f20abb9833eaafaa201f3
Vinit Solanki <2022.vinit.solanki@ves.ac.in>
2024-12-01 21:08:39 +0530

    installed the tailwind and added loader

---
+++ vyar_live_with_react_tailwind/src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className='w-screen h-screen bg-black flex flex-col justify-center items-center'>Loader</div>
+  )
+}
+
+export default Loader
